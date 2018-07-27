4ac051c96c1add535c4a0b457d3505e28237a916
Lwin Moe <lmoe@alum.mit.edu>
2018-07-26 22:39:38 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+//https://maksimivanov.com/posts/firebase-react-tutorial
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import PrivateRoute from "./PrivateRoute";
+import app from "./base";
+import Home from "./Home";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+
+
+/*class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}*/
+
+class App extends Component {
+  state = { loading: true, authenticated: false, user: null };
+
+  componentWillMount() {
+    app.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          currentUser: user,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          currentUser: null,
+          loading: false
+        });
+      }
+    });
+  }
+
+  render() {
+    const { authenticated, loading } = this.state;
+
+    if (loading) {
+      return <p>Loading..</p>;
+    }
+
+    return (
+      <Router>
+        <div>
+          <PrivateRoute
+            exact
+            path="/"
+            component={Home}
+            authenticated={authenticated}
+          />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
