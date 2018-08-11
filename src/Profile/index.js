369e65187b3205fbf2fb002f35b85a3051181328
Lineage User 1 <lineagesoftware@gmail.com>
2018-08-10 22:57:39 -0400

    Added toggleable cancel and implementated roll back to what database had. Separated into modular functions to call.

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -5,11 +5,10 @@ import firebase from "firebase";
 
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 import snackbar from "../snackbar";
 import Snackbar from '@material-ui/core/Snackbar';
@@ -22,6 +21,9 @@ import IconButton from "@material-ui/core/IconButton";
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import CardMedia from '@material-ui/core/CardMedia';
+
+//import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
     container: {
@@ -64,21 +66,33 @@ class ProfileContainer extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {name : '',
+        this.state = {userObj : null,
+                      name : '',
+                      email : '',
                       jobTitle : '',
                       education: '',
                       work: '',
                       profilePicture: null,
                       profilePictureUrl: "/generic_profile.jpg",
-                      personalEditValue : false,
+                      theInputKey: '',
+                      personalEditMode : false,
                       about : '',
-                      aboutEditValue : false,
+                      aboutEditMode : false,
                       saveNotificationOpen: false,
                       notificationVariant : '',
                       notificationMessage: ''};
     }
 
     componentWillMount(){
+        let that = this;
+        this.setUserObj(function(){
+            that.loadPersonal();
+            that.loadAbout();
+        });
+        this.setProfileUrl();
+    }
+
+    setUserObj(callback){
         let usersRef = app.database().ref('users');
         let uid = this.props.uid;
         let that = this;
@@ -86,24 +100,47 @@ class ProfileContainer extends Component {
             let exists = snap.val() !== null;
             if (exists) {
                 let userObj = snap.val();
-                that.setState({name: userObj.name});
-                that.setState({jobTitle: userObj.jobTitle});
-                that.setState({education: userObj.education});
-                that.setState({work: userObj.work});
-                that.setState({about: userObj.about});
+                that.setState({userObj: userObj});
+                if (callback) {
+                    callback();
+                }
             }
         });
-        firebase.storage().ref().child('profilePictures/' + uid).getDownloadURL().then(function(url) {
+    }
+
+    getProfilePath(){
+        let uid = this.props.uid;
+        return 'profilePictures/' + uid
+    }
+
+    setProfileUrl(){
+        let that = this;
+        firebase.storage().ref().child(this.getProfilePath()).getDownloadURL().then(function(url) {
             that.setState({profilePictureUrl: url});
         });
     }
 
-    handlePersonalEditClick = () => {
-        this.setState(state => ({ personalEditValue: true }));
-    };
+    loadPersonal(){
+        let userObj = this.state.userObj;
+        this.setState({ name: userObj.name });
+        this.setState({ email: userObj.email });
+        this.setState({ jobTitle: userObj.jobTitle });
+        this.setState({ education: userObj.education });
+        this.setState({ work: userObj.work });
+        this.setState({ profilePicture: null });
+        this.setState({ theInputKey: Math.random().toString(36) });
+    }
 
-    handleAboutEditClick = () => {
-        this.setState(state => ({ aboutEditValue: true }));
+    loadAbout(){
+        this.setState({ about: this.state.userObj.about });
+    }
+
+    handlePersonalIconClick = () => {
+        this.setState({ personalEditMode: !this.state.personalEditMode });
+        // Cancel was clicked
+        if (this.state.personalEditMode) {
+            this.loadPersonal();
+        }
     };
 
     handleNameChange = event => {
@@ -123,51 +160,72 @@ class ProfileContainer extends Component {
     };
 
     handlePersonalSubmit = async event => {
-        // TODO - Figure out how to catch errors from firebase and throw an error notification
         event.preventDefault();
         let usersRef = app.database().ref("users");
         let uid = this.props.uid;
         let userObj = usersRef.child(uid);
+        let that = this;
+        let success = true;
         userObj.update({
             name : this.state.name,
             jobTitle : this.state.jobTitle,
             education : this.state.education,
             work : this.state.work
-        });
-
-        let storageRef = firebase.storage().ref();
-        let file = this.state.profilePicture;
-        let metadata = {
-            'contentType': file.type
-        };
-        let that = this;
-        storageRef.child('profilePictures/' + uid).put(file, metadata).then(function(snapshot) {
-            //console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-            //console.log('File metadata:', snapshot.metadata);
-            snapshot.ref.getDownloadURL().then(function(url) {
-                that.setState({profilePictureUrl: url});
-            });
         }).catch(function(error) {
-            console.error('Upload failed:', error);
+            success = false;
+            that.handleOpenNotification("error", "Save of personal profile failed: " + error);
         });
 
-        this.setState(state => ({ personalEditValue: false }));
-        this.handleOpenNotification("success", "Successfully updated profile!");
+        let file = this.state.profilePicture;
+        if (file) {
+            let metadata = {
+                'contentType': file.type
+            };
+            firebase.storage().ref().child(this.getProfilePath()).put(file, metadata).then(function(snapshot) {
+                //console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+                //console.log('File metadata:', snapshot.metadata);
+                that.setProfileUrl();
+            }).catch(function(error) {
+                success = false;
+                that.handleOpenNotification("error", "Upload of profile picture failed: " + error);
+            });
+        }
+
+        this.setState({ personalEditMode: false });
+        if (success) {
+            this.handleOpenNotification("success", "Successfully updated personal profile!");
+            this.setUserObj();
+        }
     };
 
     handleAboutChange = event => {
         this.setState({ about: event.target.value });
     };
 
+    handleAboutIconClick = () => {
+        this.setState({ aboutEditMode: !this.state.aboutEditMode });
+        // Cancel was clicked
+        if (this.state.aboutEditMode) {
+            this.loadAbout();
+        }
+    };
+
     handleAboutSubmit = async event => {
-        // TODO - Figure out how to catch errors from firebase and throw an error notification
         event.preventDefault();
         let usersRef = app.database().ref("users");
+        let that = this;
+        let success = true;
         usersRef.child(this.props.uid).update({
             about : this.state.about
+        }).catch(function(error) {
+            success = false;
+            that.handleOpenNotification("error", "Save of about profile failed: " + error);
         });
-        this.setState(state => ({ aboutEditValue: false }));
-        this.handleOpenNotification("success", "Successfully updated profile!");
+        this.setState({ aboutEditMode: false });
+        if (success) {
+            this.handleOpenNotification("success", "Successfully updated about profile!");
+            this.setUserObj();
+        }
     };
 
     // notificationVariant: success info warning error
@@ -188,14 +246,38 @@ class ProfileContainer extends Component {
     handleProfilePictureChange = (event) => {
         let selectorFile = event.target.files[0];
         //console.log(selectorFile);
-        this.setState(state => ({ profilePicture: selectorFile }));
+        this.setState({ profilePicture: selectorFile });
     };
 
     render() {
         let MySnackbarContentWrapper = snackbar;
         let classes = this.props.classes;
-        let personalSaveClassName = this.state.personalEditValue ? '' : classes.hidden;
-        let aboutSaveClassName = this.state.aboutEditValue ? '' : classes.hidden;
+
+        let personalSaveClassName = classes.hidden;
+        let personalIcon = <i className="material-icons">edit</i>;
+        let nameHtml;
+        if (this.state.personalEditMode) {
+            personalSaveClassName = '';
+            personalIcon = <CancelIcon />;
+            nameHtml = <TextField
+                disabled={!this.state.personalEditMode}
+                value={this.state.name}
+                onChange={this.handleNameChange}
+            />
+        }
+        else {
+            nameHtml = <Typography variant="subheading" gutterBottom>
+                {this.state.name}
+            </Typography>
+        }
+
+        let aboutSaveClassName = classes.hidden;
+        let aboutIcon = <i className="material-icons">edit</i>;
+        if (this.state.aboutEditMode) {
+            aboutSaveClassName = '';
+            aboutIcon = <CancelIcon />;
+        }
+
         return (
             <div className={classes.container}>
                 <Grid
@@ -207,76 +289,66 @@ class ProfileContainer extends Component {
                 >
                     <Grid item>
                         <form onSubmit={this.handlePersonalSubmit} className={classes.padding}>
-                            <Card className={classes.card} raised={this.state.personalEditValue}>
+                            <Card className={classes.card} raised={this.state.personalEditMode}>
                                 <CardHeader
                                     avatar= {
-                                        <Button type="submit" variant="contained" size="small" className={personalSaveClassName} disabled={!this.state.personalEditValue}>
-                                            <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
-                                            Save
-                                        </Button>
+                                        <i className="material-icons">person</i>
                                     }
                                     action={
-                                        <IconButton onClick={this.handlePersonalEditClick}>
-                                            <i className="material-icons">edit</i>
+                                        <IconButton onClick={this.handlePersonalIconClick}>
+                                            {personalIcon}
                                         </IconButton>
                                     }
+                                    title="Personal"
                                 />
+                                <input type="file" key={this.state.theInputKey || '' } className={personalSaveClassName} disabled={!this.state.personalEditMode} onChange={this.handleProfilePictureChange} />
                                 <CardMedia
                                     className={classes.media}
                                     image={this.state.profilePictureUrl}
                                 />
                                 <CardContent>
-                                    <input type="file" className={personalSaveClassName} disabled={!this.state.personalEditValue} onChange={this.handleProfilePictureChange} />
-                                    <FormControl className={classes.formControl}>
-                                        <InputLabel htmlFor="user_name">Name</InputLabel>
-                                        <Input
-                                            id='user_name'
-                                            disabled={!this.state.personalEditValue}
-                                            value={this.state.name}
-                                            onChange={this.handleNameChange}
-                                        />
-                                    </FormControl>
-                                    <FormControl className={classes.formControl}>
-                                        <InputLabel htmlFor="job_title">Job Title</InputLabel>
-                                        <Input
-                                            id='job_title'
-                                            disabled={!this.state.personalEditValue}
-                                            value={this.state.jobTitle}
-                                            onChange={this.handleJobTitleChange}
-                                        />
-                                    </FormControl>
-                                    <FormControl className={classes.formControl}>
-                                        <InputLabel htmlFor="education">Education</InputLabel>
-                                        <Input
-                                            id='education'
-                                            disabled={!this.state.personalEditValue}
-                                            value={this.state.education}
-                                            onChange={this.handleEducationChange}
-                                        />
-                                    </FormControl>
-                                    <FormControl className={classes.formControl}>
-                                        <InputLabel htmlFor="education">Work</InputLabel>
-                                        <Input
-                                            id='work'
-                                            disabled={!this.state.personalEditValue}
-                                            value={this.state.work}
-                                            onChange={this.handleWorkChange}
-                                        />
-                                    </FormControl>
+                                    {nameHtml}
+                                    <Typography variant="subheading" gutterBottom>
+                                        {this.state.email}
+                                    </Typography>
+                                    <br />
+                                    <TextField
+                                        label="Job Title"
+                                        disabled={!this.state.personalEditMode}
+                                        value={this.state.jobTitle}
+                                        onChange={this.handleJobTitleChange}
+                                    />
+                                    <TextField
+                                        label="Education"
+                                        disabled={!this.state.personalEditMode}
+                                        value={this.state.education}
+                                        onChange={this.handleEducationChange}
+                                    />
+                                    <TextField
+                                        label="Work"
+                                        disabled={!this.state.personalEditMode}
+                                        value={this.state.work}
+                                        onChange={this.handleWorkChange}
+                                    />
+                                    <br />
+                                    <Button type="submit" variant="contained" size="small" className={personalSaveClassName} disabled={!this.state.personalEditMode}>
+                                        <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
+                                        Save
+                                    </Button>
                                 </CardContent>
                             </Card>
                         </form>
                     </Grid>
                     <Grid item>
                         <form onSubmit={this.handleAboutSubmit} className={classes.padding}>
-                            <Card className={classes.card} raised={this.state.aboutEditValue}>
+                            <Card className={classes.card} raised={this.state.aboutEditMode}>
                                 <CardHeader
                                     avatar= {
-                                        <i className="material-icons">person</i>
+                                        <i className="material-icons">person_outline</i>
                                     }
                                     action={
-                                        <IconButton onClick={this.handleAboutEditClick}>
-                                            <i className="material-icons">edit</i>
+                                        <IconButton onClick={this.handleAboutIconClick}>
+                                            {aboutIcon}
                                         </IconButton>
                                     }
                                     title="About"
@@ -288,13 +360,13 @@ class ProfileContainer extends Component {
                                             multiline
                                             rows="6"
                                             margin="normal"
-                                            disabled={!this.state.aboutEditValue}
+                                            disabled={!this.state.aboutEditMode}
                                             value={this.state.about}
                                             onChange={this.handleAboutChange}
                                         />
                                     </FormControl>
                                 </CardContent>
-                                <Button type="submit" variant="contained" size="small" className={aboutSaveClassName} disabled={!this.state.aboutEditValue}>
+                                <Button type="submit" value="save" variant="contained" size="small" className={aboutSaveClassName} disabled={!this.state.aboutEditMode}>
                                     <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
                                     Save
                                 </Button>
